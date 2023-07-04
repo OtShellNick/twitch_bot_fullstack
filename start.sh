99304f4508d2560a790501ead5948d3819e8220f
Aleksandr <exte92@gmail.com>
2023-07-04 12:51:15 +0300

    add db dump && restore sh

--- start.sh
+++ start.sh
@@ -7,3 +7,7 @@ docker rm -f $(docker ps -a -q)
 docker volume rm $(docker volume ls -q)
 
 docker-compose up -d
+
+docker compose exec -i mongo1 sh -c 'mongorestore --archive' < ./backups/db.archive
+
+sleep 5
--- stop.sh
+++ stop.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+mkdir ./backups
+cd ./backups
+touch ./db.archive
+
+sleep 5
+
+docker compose exec -T mongo1 sh -c 'mongodump  --db=witch-database --archive' > ./db.archive
+
 docker-compose down
 docker rm -f $(docker ps -qa)
 docker rmi -f $(docker images -aq)
