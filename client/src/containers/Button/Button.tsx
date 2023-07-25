8094437c94a9369f9ab398dcc6af3ae408c08d2e
Aleksandr <exte92@gmail.com>
2023-07-25 14:23:46 +0300

    add type && size to button

--- client/src/containers/Button/Button.tsx
+++ client/src/containers/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
 import cl from 'classnames';
 
 import './Button.scss';
@@ -11,11 +11,21 @@ enum ColorsENUM {
   disabled = 'disabled',
 }
 
+enum SizesENUM {
+  sm = 'sm',
+  md = 'md',
+  lg = 'lg',
+}
+
 interface ButtonProps {
   children: React.ReactNode;
   color?: ColorsENUM;
   disabled?: boolean;
   onClick?: () => void;
+  size?: SizesENUM;
+  iconBefore?: React.ReactNode;
+  iconAfter?: React.ReactNode;
+  type?: DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>['type'];
 }
 
 export const Button: React.FC<ButtonProps> = ({
@@ -23,6 +33,10 @@ export const Button: React.FC<ButtonProps> = ({
   disabled = false,
   color = 'primary',
   onClick,
+  size = 'md',
+  iconAfter,
+  iconBefore,
+  type,
 }) => {
   return (
     <button
@@ -30,9 +44,14 @@ export const Button: React.FC<ButtonProps> = ({
         btn: true,
         [`btn-${color}`]: true,
         [`btn-disabled`]: disabled,
+        [`btn-${size}`]: true,
       })}
-      onClick={onClick}>
+      onClick={onClick}
+      disabled={disabled}
+      type={type || 'button'}>
+      {iconBefore && iconBefore}
       {children}
+      {iconAfter && iconAfter}
     </button>
   );
 };
