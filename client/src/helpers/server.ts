7320725b8949c06aa7938ce350f4b426f6fc764d
Aleksandr <exte92@gmail.com>
2023-08-03 19:37:34 +0300

    temp fix server

--- client/src/helpers/server.ts
+++ client/src/helpers/server.ts
@@ -45,54 +45,55 @@ class HttpService implements IHttpService {
    * Установить перехватчик ответов для обработки обновления токена
    * @method setResponseInterceptor
    */
-  setResponseInterceptor(): void {
+  async setResponseInterceptor(): Promise<void> {
     this.axiosInstance.interceptors.response.use(
-      response => {
+      async response => {
         const {
           headers: { authorization },
           data: { data },
         } = response;
 
+        console.log('@@response', response);
+
         if (authorization) {
-          localStorage.setItem('aubottok', authorization);
+          await localStorage.setItem('aubottok', authorization);
         }
 
         return { ...response, data };
       },
-      error => {
-        console.log('@@error', error);
-        if (
-          error.response?.status &&
-          (error.response.status === 403 || error.response.status === 401)
-        ) {
-          if (!localStorage.getItem('aubottok') && location.pathname !== '/login') {
-            location.replace('/login');
-            return Promise.reject(error);
-          }
+      async error => {
+        console.log('@@error', error.response);
+        return error;
+        // if (
+        //   error.response?.status &&
+        //   (error.response.status === 403 || error.response.status === 401)
+        // ) {
+        //   if (!localStorage.getItem('aubottok') && location.pathname !== '/login') {
+        //     location.replace('/login');
+        //     return Promise.reject(error);
+        //   }
 
-          if (this.retries > 0) {
-            this.axiosInstance
-              .request({
-                method: 'POST',
-                url: `auth/refresh`,
-                data: {
-                  token: localStorage.getItem('aubottok'),
-                },
-              })
-              .then(response => {
-                localStorage.setItem('aubottok', response.data);
-                this.retries = 3;
-                // TODO: Добавить эмиттер на RxJS об успешном обновлении токена
-              })
-              .catch(_error => {
-                localStorage.removeItem('aubottok');
-                location.replace('/login');
-                return Promise.reject(_error);
-              });
-            this.retries--;
-          }
-        }
-        return Promise.reject(error);
+        //   if (this.retries > 0) {
+        //     try {
+        //       const refreshResponse = await this.axiosInstance.request({
+        //         method: 'POST',
+        //         url: `auth/refresh`,
+        //         data: {
+        //           token: localStorage.getItem('aubottok'),
+        //         },
+        //       });
+        //       await localStorage.setItem('aubottok', refreshResponse.data);
+        //       this.retries = 3;
+        //       // TODO: Добавить эмиттер на RxJS об успешном обновлении токена
+        //     } catch (_error) {
+        //       localStorage.removeItem('aubottok');
+        //       location.replace('/login');
+        //       return Promise.reject(_error);
+        //     }
+        //     this.retries--;
+        //   }
+        // }
+        // return Promise.reject(error);
       },
     );
   }
